888f36000f1de6429d82e7059e724d7958065f02
milka77 <55999046+milka77@users.noreply.github.com>
2020-07-30 10:24:30 +0100

    Added notification css, shopping bag preview, and additional messages

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,14 +7,14 @@ def  bag_contents(request):
 
     bag_items = []
     total = 0
-    procduct_count = 0
+    product_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
-            procduct_count += item_data
+            product_count += item_data
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
@@ -24,7 +24,7 @@ def  bag_contents(request):
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
-                procduct_count += quantity
+                product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
@@ -45,7 +45,7 @@ def  bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'procduct_count': procduct_count,
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
