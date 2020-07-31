1b5e66a514ca18d1428a077b48b4f4b353c3f4ab
milka77 <55999046+milka77@users.noreply.github.com>
2020-07-31 10:02:45 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HAtaMLXFpruDvBntY9jHWkbbTmXV9AMQQdhLd6sSIUeerYb1Fyd7yi8qEyAV3YH6PFIDzDvtlhbuQlJr2yeUsyT00RaOcrzkL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
