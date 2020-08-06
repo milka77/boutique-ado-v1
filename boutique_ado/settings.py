05b05142ef3ca0c38551f149d76d9931687694e2
milka77 <krisztian.keseru@gmail.com>
2020-08-06 09:15:35 +0000

    added profiles app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-#import env as config
+# import env as config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
