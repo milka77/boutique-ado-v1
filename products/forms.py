6955b43fc5846cdc8396c6fc417152820fdf907d
milka77 <krisztian.keseru@gmail.com>
2020-08-10 11:57:44 +0000

    Product admin - fixing the image input

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
-from  django import forms
+from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -8,11 +9,13 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
-            self.fields['category'].choices = friendly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
