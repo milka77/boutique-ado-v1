0305cd74499c05f398ec05b0b9b32bcd5063958b
milka77 <krisztian.keseru@gmail.com>
2020-08-13 21:31:22 +0000

    added development to env

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,9 +185,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorages'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorages'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
